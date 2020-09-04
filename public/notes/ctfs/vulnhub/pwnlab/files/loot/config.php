cb6ab08d02019001f3398d4ca73569db77571ddc
Justin-p <justin-p@users.noreply.github.com>
2020-09-04 16:17:18 +0200

    removed public

--- public/notes/ctfs/vulnhub/pwnlab/files/loot/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$server	  = "localhost";
-$username = "root";
-$password = "H4u%QJ_H99";
-$database = "Users";
-?>
--- public/notes/programming/powershell/remove_default_db.ps1
+++
@@ -1,9 +0,0 @@
-```powershell
-Get-Mailbox –Database “Mailbox Database 0923333700”
-Get-Mailbox –Database “Mailbox Database 0923333700” |New-MoveRequest –TargetDatabase “DAG”  -BatchName "DB01toDB02"
-
-Get-Mailbox -Database “Mailbox Database 0923333700” | New-MoveRequest -TargetDatabase “DAG” -BatchName "DB01toDB02"
-
-Get-Mailbox –Arbitration
-Get-Mailbox –Arbitration | New-MoveRequest –TargetDatabase "TargetDatabaseName"
-```
